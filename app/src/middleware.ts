894f069a0f06c29ee42fa0ee58590d0648a5e383
Andre Lavell <andrelavell@gmail.com>
2025-04-18 10:47:44 -0700

    Refactor: improve middleware cookie handling

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -23,11 +23,6 @@ export async function middleware(request: NextRequest) {
             value,
             ...options,
           })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
           response.cookies.set({
             name,
             value,
@@ -40,11 +35,6 @@ export async function middleware(request: NextRequest) {
             name,
             value: '',
             ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
           })
           response.cookies.set({
             name,
